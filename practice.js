97c7659a370793a50c222c535dfecdb2bf790c99
Aritra Dey <deyaritra1998@gmail.com>
2025-06-26 00:05:53 +0530

    Day 1 commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+console.log('Hello');
+let a = 5;
+let b = 6;
+function calc(){
+    let c = a+b;
+    console.log(c);
+}
+const print = calc();
+console.log(print);
---
+++ test.js
@@ -0,0 +1,6 @@
+// console.log(1+2);
+// console.log("Hello");
+var a = 9;
+var b = 7;
+var c = a+b;
+console.log(c);
